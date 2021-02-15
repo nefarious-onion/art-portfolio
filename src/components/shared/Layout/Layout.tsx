99a295f5970502a1f73da602684cdfcdc013f879
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-15 15:53:20 +0200

    pass data as props to navigation, add styles and types for props

--- src/components/shared/Layout/Layout.tsx
+++ src/components/shared/Layout/Layout.tsx
@@ -1,14 +1,43 @@
 import Footer from '@shared/Footer/Footer';
-import Navigation from '@shared/Navigation/Navigation';
+import Navigation, { NavData } from '@shared/Navigation/Navigation';
 import Head from 'next/head';
 
-const Layout = ({ children }) => {
+export type SiteData = {
+  __typename: string,
+  title: string
+  metadata: {
+    title: string
+    description: string
+  }
+  applicationTexts: {
+    navigation: NavData
+  }
+  socialMediaLinks: {
+    facebook: string
+    youtube: string
+    instagram: string
+  }
+  footer: {
+    copyright: string
+  }
+}
+
+type LayoutProps = {
+  siteData: SiteData
+}
+
+const Layout: React.FC<LayoutProps> = ({ siteData, children }) => {
+  const { metadata, applicationTexts, socialMediaLinks } = siteData
+
   return (
     <>
       <Head>
-        <title>muutos company</title>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
       </Head>
-      <Navigation />
+      <div className="'absolute top-0 left-0 w-full h-24 pb-0 px-4 z-50 s-20 bg-black">
+        <Navigation navData={applicationTexts.navigation} />
+      </div>
       <main>{children}</main>
       <Footer />
     </>
