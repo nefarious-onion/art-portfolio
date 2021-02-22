f28a738beee05ba3526b5056ed0e72ec8e12af5b
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-22 15:34:24 +0200

    Implement new interfaces to layout component, add headetText to Layout in order to make it sticky on  mobile view

--- src/components/shared/Layout/Layout.tsx
+++ src/components/shared/Layout/Layout.tsx
@@ -1,73 +1,62 @@
 import { useState } from 'react';
 import Head from 'next/head';
-import TopNav, { NavData } from '@shared/Navigation/TopNav';
+import TopNav from '@shared/Navigation/TopNav';
 import NavMenu from '@shared/Navigation/NavMenu';
 import Footer from '@shared/Footer/Footer';
-
-export type SiteData = {
-  __typename: string,
-  title: string
-  metadata: {
-    title: string
-    description: string
-  }
-  applicationTexts: {
-    navigation: NavData
-  }
-  socialMediaLinks: {
-    facebook: string
-    youtube: string
-    instagram: string
-  }
-  footer: {
-    copyright: string
-  }
-}
+import { GetPageDataResult } from 'queries/page';
 
 type LayoutProps = {
-  siteData: SiteData
+  siteData: GetPageDataResult['pageCollection']['items'][0]
   headerText: string
 }
 
 const Layout: React.FC<LayoutProps> = ({ siteData, children, headerText }) => {
   const [isVisible, setIsVisible] = useState(false)
-  const { metadata, applicationTexts, socialMediaLinks } = siteData
+  const { pageTexts } = siteData
 
   const toggleMobileMenu = () => {
     setIsVisible(!isVisible)
   }
+  const sharedHeaderClasses = 'laptop:text-4xl tablet:text-3xl mobile:text-2xl antialiased'
 
   return (
     <>
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>{pageTexts.metadata.title}</title>
+        <meta name="description" content={pageTexts.metadata.description} />
       </Head>
-      <header className="bg-black">
-        <TopNav
-          navData={applicationTexts.navigation}
-          headerText={headerText}
-        />
-        <NavMenu
-          navData={applicationTexts.navigation}
-          toggleMobileMenu={toggleMobileMenu} isVisible={isVisible}
-        />
-        <div
-          onClick={toggleMobileMenu}
-          className="w-8 bg-black py-2 tablet:invisible fixed right-0 top-0  mr-4 mt-5">
-          <svg
-            x="0px"
-            y="0px"
-            viewBox="0 0 30 14.8"
-            className='fill-current text-fullMint ...'
-          >
-            <rect y="10.8" width="30" height="4" />
-            <rect x="15" width="15" height="4" />
-          </svg>
-        </div>
-      </header>
-      <main>{children}</main>
-      <Footer />
+      <div className="laptop:container mx-auto ">
+        <header>
+          <TopNav
+            headerText={headerText}
+          />
+          <NavMenu
+            navData={pageTexts.navigation}
+            toggleMobileMenu={toggleMobileMenu}
+            isVisible={isVisible}
+          />
+          <div
+            onClick={toggleMobileMenu}
+            className="w-12 bg-black py-2 tablet:invisible fixed right-0 top-0 p-2 mr-4 mt-8 z-30 rounded-sm">
+            <svg
+              x="0px"
+              y="0px"
+              viewBox="0 0 30 14.8"
+              className='fill-current text-fullMint ...'
+            >
+              <rect y="10.8" width="30" height="4" />
+              <rect x="15" width="15" height="4" />
+            </svg>
+          </div>
+        </header>
+        <main >
+          <div className={`${sharedHeaderClasses} bg-fullMint text-black tablet:text-right text-center pr-4 tablet:w-2/4 laptop:w-2/4 tracking-widest text-2xl mobile:text-3xl font-medium py-4 z-10 sticky top-0 ... tablet:relative`}>
+            {headerText}
+          </div>
+          {children}
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
