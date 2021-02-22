4f624c56188cece921e80049c5e2c21df755fc42
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-22 15:31:15 +0200

    refactor navmenu to make it more readable

--- src/components/shared/Navigation/NavMenu.tsx
+++ src/components/shared/Navigation/NavMenu.tsx
@@ -1,41 +1,21 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { NavData } from './TopNav';
+import LinkItem from './LinkItem';
 
 type NavMenuProps = {
-  navData: NavData
+  navData: { [key: string]: string }
   toggleMobileMenu: () => void
   isVisible: boolean
 }
 
-type LinkItemProps = {
-  href: string
-}
-
-const LinkItem: React.FC<LinkItemProps> = ({ children, href }) => {
-  const router = useRouter()
-  const isActive = router.pathname === href
-  const activeClass = isActive ? 'text-fullPink' : ''
-  const transitionClasses = 'transition-colors duration-150 ease-in-out '
-  const menuItemClasses = `block mobile:py-4 tablet:py-1 tablet:px-4 laptop:hover:bg-fullMint laptop:hover:text-black ${transitionClasses} ${activeClass}`
-  return (
-    <Link href={href}>
-      <a
-        className={menuItemClasses} >
-        {children}
-      </a>
-    </Link>
-  )
-}
-
 const NavMenu: React.FC<NavMenuProps> = ({ navData, toggleMobileMenu, isVisible }) => {
-  const mobileVisibility = isVisible ? 'mobile:visible' : 'mobile:invisible'
-  const mobileClasses = `${mobileVisibility} mobile:h-screen mobile:w-screen mobile:pt-40  mobile:text-3xl text-center`
-  const tabletAndUpClasses = 'laptop:text-3xl tablet:text-2xl tablet:text-right tablet:mr-4 tablet:mt-8 tablet:border-r-4 tablet:border-paleGreen laptop:hover:border-fullMint'
-  const sharedClasses = 'fixed right-0 top-0 leading-10 bg-black antialiased'
+  const mobileVisibility = isVisible ? 'visible' : 'invisible'
+
+  const mobileClasses = `${mobileVisibility} h-screen w-screen pt-40  text-3xl text-center`
+
+  const tabletAndUpClasses = 'tablet:visible tablet:h-auto tablet:w-auto tablet:pt-0 laptop:text-3xl tablet:text-2xl tablet:text-right tablet:mr-4 tablet:mt-8 tablet:border-r-4 tablet:border-paleGreen laptop:hover:border-fullMint'
+
+  const sharedClasses = 'fixed right-0 top-0 leading-10 bg-black antialiased z-20'
 
   return (
-
     <ul className={`${sharedClasses} ${mobileClasses} ${tabletAndUpClasses}`}>
       <li >
         <LinkItem href='/'>{navData.home}</LinkItem>
@@ -51,7 +31,7 @@ const NavMenu: React.FC<NavMenuProps> = ({ navData, toggleMobileMenu, isVisible 
       </li>
     </ul>
 
-  );
+  )
 }
 
-export default NavMenu;
+export default NavMenu
