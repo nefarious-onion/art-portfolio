a599ae3caa036385e90c25aec600252a537b53c8
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-25 16:10:36 +0200

    rename topnav to logo

---
+++ src/components/shared/Navigation/Logo.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Logo: React.FC = () => {
+  return (
+    <Link href='/'>
+      <a className='inline-block uppercase text-2xl mobile:text-3xl  tablet:text-5xl font-extrabold tracking-widest font-logo text-white'>
+        muutos company
+      </a>
+    </Link>
+  );
+}
+
+export default Logo;
