4d8f13a1dd7c5a49f939ad3629604fc26e2b83bb
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-25 17:21:26 +0200

    add icon to library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,8 @@ import { apolloClient } from 'setup/apolloClient'
 import '../styles/globals.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fab)
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faTimes)
 
 function MyApp({ Component, pageProps }) {
 
