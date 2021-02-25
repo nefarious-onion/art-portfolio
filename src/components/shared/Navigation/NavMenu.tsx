1aa7fcfb90e5531fc880ccc1d6a34def77c53921
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-25 17:26:04 +0200

    add icon and logic for closing the menu, add styles for bigger screens

--- src/components/shared/Navigation/NavMenu.tsx
+++ src/components/shared/Navigation/NavMenu.tsx
@@ -1,26 +1,27 @@
 import { useRouter } from 'next/router'
 import { Site } from 'queries/page';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 //components
 import LinkItem from './LinkItem';
 import LocaleSwitcher from './LocaleSwitcher';
 
 type NavMenuProps = {
   navData: Site['navigation']
-  toggleMobileMenu: () => void
+  toggleMenu: () => void
   isVisible: boolean
 }
 
 const NavMenu: React.FC<NavMenuProps> = ({
   navData,
-  toggleMobileMenu,
+  toggleMenu,
   isVisible
 }) => {
   const { pathname, locale, locales } = useRouter()
   const menuVisibility = isVisible ? 'visible translate-x-0' : 'invisible translate-x-full'
   const transformClasses = 'ease-in-out ransition-all duration-300 transform top-0 right-0'
-  const sharedClasses = `${menuVisibility} ${transformClasses} leading-10 bg-black z-20 fixed right-0 top-0 h-screen`
-  const mobileClasses = 'w-screen pt-40 text-3xl text-center tablet:text-4x'
-  const laptopAndUpClasses = 'laptop:w-1/4 laptop:pt-24 laptop:text-2xl laptop:text-right laptop:mr-4 laptop:border-r-4 laptop:border-paleGreen laptop:hover:border-fullMint'
+  const sharedClasses = `${menuVisibility} ${transformClasses} leading-10 bg-black z-40 fixed right-0 top-0 h-screen`
+  const mobileClasses = 'w-screen text-3xl text-center tablet:text-4x'
+  const laptopAndUpClasses = 'desktop:w-1/4 laptop:w-1/3 laptop:pt-0 laptop:text-2xl laptop:text-right laptop:mr-4 laptop:border-r-4 laptop:border-paleGreen laptop:hover:border-fullMint'
 
 
   const renderedNavList = navData.map(item => <li key={item.title}>
@@ -29,7 +30,15 @@ const NavMenu: React.FC<NavMenuProps> = ({
 
   return (
     <div className={`${sharedClasses} ${mobileClasses} ${laptopAndUpClasses}`}>
-      <ul >
+
+      <FontAwesomeIcon
+        onClick={toggleMenu}
+        icon="times"
+        className='text-fullPink float-left ml-10 mt-12 text-4xl'
+      />
+
+
+      <ul className='mt-40 laptop:mt-24' >
         {renderedNavList}
       </ul>
       <LocaleSwitcher pathname={pathname} currentLocale={locale} locales={locales} />
