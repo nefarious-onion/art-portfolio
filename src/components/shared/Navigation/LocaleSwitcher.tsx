1d944cc97e3622d146e203b3bed5df64e16a2a47
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-25 12:40:55 +0200

    create localeSwitcher

---
+++ src/components/shared/Navigation/LocaleSwitcher.tsx
@@ -0,0 +1,25 @@
+
+import Link from 'next/link'
+
+interface LocaleSwitcherProps {
+  pathname: string
+  currentLocale: string
+  locales: string[]
+}
+
+const LocaleSwitcher: React.FC<LocaleSwitcherProps> = ({ pathname, currentLocale, locales }) => {
+  const linkClasses = 'laptop:hover:bg-fullPink'
+
+  const links = locales.map(locale => <Link href={pathname} locale={locale}>
+    <a className={locale === currentLocale ? 'text-fullPink border-b-2 border-fullPink' : ''}>
+      {locale}
+    </a>
+  </Link>)
+  return (
+    <div className='tablet:mr-4 text-xl uppercase text-center space-x-12 mt-20'>
+      {links}
+    </div>
+  );
+}
+
+export default LocaleSwitcher;
