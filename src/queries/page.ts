fd8eb070e39465e329bf3cd0faad2faaec7ad622
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-25 12:39:39 +0200

    add interfaces for page text content

--- src/queries/page.ts
+++ src/queries/page.ts
@@ -1,41 +1,72 @@
 import { gql } from '@apollo/client';
 import { Asset } from './projects';
 
-export interface Page {
-  title: string
-  pageTexts: {
-    [key: string]: {
-      [key: string]: string
-    }
+export interface About {
+  headers: {
+    title: string
   }
+}
+export interface Projects {
+  headers: {
+
+  }
+}
+export interface Project {
+  headers: {
+
+  }
+}
+export interface Contact {
+  headers: {
+    header1: string
+    header2: string
+  }
+  contactForm: {
+    name: string
+    send: string
+    email: string
+    message: string
+    subject: string
+    contact_title: string
+    yup_required: string
+    yup_error_name: string
+    yup_error_email: string
+    yup_error_message: string
+  }
+}
+export interface Site {
+  footer: {
+    copyright: string
+  }
+  metadata: {
+    title: string
+    description: string
+  }
+  navigation: NavObj[]
+}
+type NavObj = {
+  title: string
+  href: string
+}
+export interface Home {
+  hero: {
+    hero1: string
+    hero2: string
+    hero3: string
+    hero4: string
+    hero6: string
+    hero7: string
+  }
+}
+
+export interface Page<T> {
+  title: string
+  pageTexts: T
   longText: string
   pageImagesCollection: {
     items: Asset[]
   }
 }
-
-// export const GET_PAGE_IMAGE = gql`
-// query getPageImage($title: String!) {
-//   pageCollection(where: {
-//     title: $title
-//   }) {
-//     items {
-//       pageImagesCollection {
-//         items {
-//           title
-//           url
-//           width
-//           height
-//         }
-//       }
-//     }
-//   }
-// `
-// export interface GetPageImageResult {
-//   pageCollection: {
-//     items: Pick<Page, 'pageImageCollection'>
-//   }
-// }
 
 export const GET_PAGE_DATA = gql`
 query getPageData($title: String!, $locale: String!) {
@@ -58,9 +89,9 @@ query getPageData($title: String!, $locale: String!) {
   }
 }
 `
-export interface GetPageDataResult {
+export interface GetPageDataResult<T> {
   pageCollection: {
-    items: Page[]
+    items: Page<T>[]
   }
 }
 
