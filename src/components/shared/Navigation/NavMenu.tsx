4f53a5faaa748366f8f36207bb506225ff8dcd90
anne k <50986817+nefarious-onion@users.noreply.github.com>
2021-02-25 16:17:03 +0200

    redesign navmenu component to not be visible on bigger screen sizes, add transformClasses and rename variables

--- src/components/shared/Navigation/NavMenu.tsx
+++ src/components/shared/Navigation/NavMenu.tsx
@@ -16,13 +16,11 @@ const NavMenu: React.FC<NavMenuProps> = ({
   isVisible
 }) => {
   const { pathname, locale, locales } = useRouter()
-  const mobileVisibility = isVisible ? 'visible' : 'invisible'
-
-  const sharedClasses = 'fixed right-0 top-0 leading-10 bg-black antialiased z-20'
-
-  const mobileClasses = `${mobileVisibility} h-screen w-screen pt-40 text-3xl text-center`
-
-  const tabletAndUpClasses = 'tablet:visible tablet:h-auto tablet:w-auto tablet:pt-0 laptop:text-3xl tablet:text-2xl tablet:text-right tablet:mr-4 tablet:mt-8 tablet:border-r-4 tablet:border-paleGreen laptop:hover:border-fullMint'
+  const menuVisibility = isVisible ? 'visible translate-x-0' : 'invisible translate-x-full'
+  const transformClasses = 'ease-in-out ransition-all duration-300 transform top-0 right-0'
+  const sharedClasses = `${menuVisibility} ${transformClasses} leading-10 bg-black z-20 fixed right-0 top-0 h-screen`
+  const mobileClasses = 'w-screen pt-40 text-3xl text-center tablet:text-4x'
+  const laptopAndUpClasses = 'laptop:w-1/4 laptop:pt-24 laptop:text-2xl laptop:text-right laptop:mr-4 laptop:border-r-4 laptop:border-paleGreen laptop:hover:border-fullMint'
 
 
   const renderedNavList = navData.map(item => <li key={item.title}>
@@ -30,7 +28,7 @@ const NavMenu: React.FC<NavMenuProps> = ({
   </li>)
 
   return (
-    <div className={`${sharedClasses} ${mobileClasses} ${tabletAndUpClasses}`}>
+    <div className={`${sharedClasses} ${mobileClasses} ${laptopAndUpClasses}`}>
       <ul >
         {renderedNavList}
       </ul>
